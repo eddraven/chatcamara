b70ffc6d11f6307deb7b01a2ee62348ff5190aeb
eddraven <eduardohr91@hotmail.com>
2019-01-15 10:08:00 -0500

    subiendo chat camara

---
+++ webchat/public/js/chat.js
@@ -0,0 +1,50 @@
+var socket = io.connect();
+var $messageForm = $('#send-message');
+var $messageBox = $('#message');
+var $chat = $('#chat');
+var $buttonSend = $('#send');
+
+var $nickForm = $('#setNick');
+var $nickBox = $('#nickname');
+var $users = $('#users');
+var $closeAlert = $('#closeAlert');
+
+var chat = function(e) {
+   e.preventDefault();
+   socket.emit('new user', $nickBox.val(), function(data) {
+       if(data) {
+           $('.nickWrap').hide();
+           $('.contentWrap').show();
+       } else {
+           $("#login-error").show();
+       }
+   });
+   $nickBox.val('');
+   $("h1").text("¡Bienvenido al chat!");
+}
+var iniciar = function($) {
+   $nickForm.click(chat);
+   $closeAlert.click(function(e) {
+        $("#login-error").hide();
+   });
+   $messageForm.submit(function(e) {
+       e.preventDefault();
+       if($messageBox.val()!='') socket.emit('send message', $messageBox.val());
+       $messageBox.val('');
+   });
+   
+   socket.on('new message', function(data) {
+      var $msn = $("<div>");
+      $msn.append("<b>"+data.nick+":</b> "+data.msg + "</br>");
+      $chat.append($msn);
+   });
+   
+   socket.on('usernames', function(data) {
+        var html = '';
+        for (var username in data) {
+            html += username + "</br>";
+        }
+        $users.html(html);
+    });
+};
+jQuery(iniciar);
---
+++ webchat/server.js
@@ -0,0 +1,46 @@
+/*var express = require ("express");
+var app = express();
+app.use(express.static(__dirname + "/public")) 
+app.listen(1717)*/
+
+//CHAT
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require("socket.io").listen(server);
+var nicknames = {};
+
+app.set("port", process.env.PORT || 8000);
+
+app.use(express.static(__dirname + '/public'));
+
+io.sockets.on('connection', function(socket) {
+    socket.on('send message', function(data) {
+        console.log(data);
+        io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+    });
+    
+    socket.on('new user', function(data, callback) {
+        if (data in nicknames) {
+            callback(false);
+        } else {
+            callback(true);
+            socket.nickname = data;
+            nicknames[socket.nickname] = 1;
+            updateNickNames();
+        }
+    });
+    
+    socket.on('disconnect', function(data) {
+        if(!socket.nickname) return;
+        delete nicknames[socket.nickname];
+        updateNickNames();
+    });
+    
+    function updateNickNames() {
+        io.sockets.emit('usernames', nicknames);
+    }
+});
+server.listen(app.get("port"), function() {
+    console.log("Servidor encendido 8000");
+});
